af8f7d2b1ec65a720dc2c6f5c65e520a6d783388
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-20 10:04:32 +0100

    profile page has info

--- src/pages/PageProfile.tsx
+++ src/pages/PageProfile.tsx
@@ -1,10 +1,17 @@
-import { useContext } from "react"
-import { AppContext } from "../AppContext"
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
 
 export const PageProfile = () => {
 	const { currentUser } = useContext(AppContext);
 
 	return (
-		<p>{currentUser.fullName}</p>
-	)
-}
+		<div className="flex gap-3">
+			<img className="w-[7rem]" src="images/users/defaultProfile.jpg" />
+			<div>
+				<p>Full name: {currentUser.fullName}</p>
+				<p>Login: {currentUser.login}</p>
+				<p>Email: {currentUser.email}</p>
+			</div>
+		</div>
+	);
+};
